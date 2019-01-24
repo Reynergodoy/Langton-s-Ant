e72b8e88b822b5bf6f32236547e075e325170c69
Rey <reyneraccel@gmail.com>
2019-01-24 13:04:41 -0200

    Update ant.js

--- ant.js
+++ ant.js
@@ -1,7 +1,29 @@
 export class Ant {
-    constructor (rules, x, y) {
-        this.rules = rules;
-        this.x = x;
-        this.y = y;
+    constructor (colors, set, pos, dimensions) {
+        this.colors = colors;
+        this.set = set;
+        this.rules = {};
+        this.pos = pos;
+        this.dimensions = dimensions;
+        this.state = [];
+    }
+    step () {
+        
+    }
+    parseRules () {
+        let rules = this.rules;
+        let set = this.set;
+        const colors = this.colors;
+        const amount = set.length;
+        
+        if (typeof set === 'string') set = set.split('');
+        
+        if (amount !== colors.length) return false;
+        
+        for (let i = 0; i < amount; i++) {
+            rules[colors[i]] = set[i];
+        }
+        
+        return true;
     }
 }
